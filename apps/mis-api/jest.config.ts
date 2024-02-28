1ebd56aab20eb16ce031a72c3c780de479bd17ae
letovsk1y <lihod07@gmail.com>
2024-02-28 08:49:07 +0700

    fix(mis-api): rename apps pet-mis & pet-mis-e2e to mis-api & mis-api-e2e

--- apps/mis-api/jest.config.ts
+++ apps/mis-api/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'pet-mis',
+  displayName: 'mis-api',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/pet-mis',
+  coverageDirectory: '../../coverage/apps/mis-api',
 };
